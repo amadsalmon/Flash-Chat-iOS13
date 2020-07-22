44735c1e7eb8d9c7d9ebe8f6fe5ec5b56a6936ab
Amad Salmon <amad.salmon@icloud.com>
2020-07-23 01:15:00 +0200

    refactored with a Constants static structure file

--- Flash Chat iOS13/Controllers/RegisterViewController.swift
+++ Flash Chat iOS13/Controllers/RegisterViewController.swift
@@ -25,7 +25,7 @@ class RegisterViewController: UIViewController {
                         print(e.localizedDescription)
                     } else {
                         self.errorLabel.isHidden = true
-                        self.performSegue(withIdentifier: "RegisterToChat", sender: self)
+                        self.performSegue(withIdentifier: Constants.registerSegue, sender: self)
                     }
                 }
             } else{
