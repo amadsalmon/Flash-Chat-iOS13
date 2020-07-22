aa70b0f1dd1ee1cfc0239e60757491707b014ccd
Amad Salmon <amad.salmon@icloud.com>
2020-07-23 00:04:04 +0200

    Added user registration support

--- Flash Chat iOS13/Controllers/RegisterViewController.swift
+++ Flash Chat iOS13/Controllers/RegisterViewController.swift
@@ -7,13 +7,32 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class RegisterViewController: UIViewController {
 
     @IBOutlet weak var emailTextfield: UITextField!
     @IBOutlet weak var passwordTextfield: UITextField!
+    @IBOutlet weak var errorLabel: UILabel!
     
     @IBAction func registerPressed(_ sender: UIButton) {
+        if let emailAddress = emailTextfield.text {
+            if let password = passwordTextfield.text{
+                Auth.auth().createUser(withEmail: emailAddress, password: password) { (authResult, error) in
+                    if let e = error {
+                        self.errorLabel.isHidden = false
+                        self.errorLabel.text = "Error: \(e.localizedDescription)"
+                        print(e.localizedDescription)
+                    } else {
+                        self.performSegue(withIdentifier: "RegisterToChat", sender: self)
+                    }
+                }
+            } else{
+                print("Please enter password.")
+            }
+        } else{
+            print("Please enter email address.")
+        }
     }
     
 }
